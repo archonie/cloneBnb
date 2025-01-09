168c727c9cb2fb927e38e77f96475680c79c49f1
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2025-01-09 22:31:58 +0300

    Initial Commit

---
+++ AirBnbClone/AirBnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnbCloneApp.swift
+//  AirBnbClone
+//
+//  Created by Doğan Ensar Papuçcuoğlu on 9.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AirBnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
